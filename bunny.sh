8f55005b9ca7d017ec694a35ca572dbcb3cddafd
alemosie <alexandra.siega@gmail.com>
2021-10-23 23:38:10 +0200

    "Update bunny to add, commit, and push on yeet"

--- bunny.sh
+++ bunny.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-string=$(cat << EOF
-|------------------|
-| Git Bunny says:  | 
-| $1            |
-|------------------|
-(\__/) ||
-(•ㅅ•) ||
-/ 　 づ
+sign=$(cat << EOF
+  
+     |------------|
+     | PUSH IT.   |
+     | PUSH IT    |
+     | REAL GOOD. |
+     |------------|
 EOF
 )
 
-echo "$string"
+bunny=$(cat << EOF
+   ${sign}
+   (\__/) ||
+   (•ㅅ•) ||
+   / 　 づ
+EOF
+)
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+message=$1
+
+echo "You're good to commit all your changes and yeet them to origin/$branch?"
+read -n 2 -p "Y/n: " input
+
+if [ "$input" = "Y" ] ; then
+  echo "$bunny"
+  
+  git add . && \
+  git status && \
+  git commit -m "\"$message\""
+  git push origin $branch
+fi
