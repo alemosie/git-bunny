864d427d660680208b7ae0897bd3fb080b3233a8
alemosie <alexandra.siega@gmail.com>
2021-10-24 14:17:18 +0200

    "Add setup + README"

--- bunny_setup.sh
+++ bunny_setup.sh
@@ -11,12 +11,14 @@ EOF
 )
 
 echo "$bunny"
-echo "`git yeet` will add, commit, and push all active git changes to the origin branch."
+echo "\`git yeet\` will add, commit, and push"
+echo "all active git changes to the origin branch."
 
 bunny_directory=$(pwd)
 bunny_file_path="$bunny_directory/bunny.sh"
 
-# git config --global alias.yeet "!sh $bunny_file_path"
+git config --global alias.yeet "!sh $bunny_file_path"
 
-echo "You now have access to `git yeet` as an alias 🚀"
-echo "Execute `git yeet --help` to learn more."
+echo ""
+echo "You now have access to \`git yeet\` as an alias 🚀"
+echo "Execute \`git yeet --help\` to learn more."
